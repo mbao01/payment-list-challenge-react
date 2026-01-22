78d88ae863b192f49763cb1dec20c58a6d9fa2f3
Ayomide Bakare <bakkareayomideo@gmail.com>
2026-01-22 17:51:16 +0000

    format date utility fn and extend test api

---
+++ src/utilities/dates/formatDate.ts
@@ -0,0 +1,11 @@
+import { format } from "date-fns/format";
+import { isValid } from "date-fns/isValid";
+
+export const formatDate = (
+  date: Date | string | null | undefined,
+  dateFormat = "dd/MM/yyyy, HH:mm:ss",
+) => {
+  if (!(date && isValid(date))) return null;
+
+  return format(date, dateFormat);
+};
--- vitest.config.ts
+++ vitest.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
     outputFile: {
       junit: "./junit.xml",
     },
+    setupFiles: ["./vitest-setup.ts"],
     coverage: {
       provider: "v8",
       reporter: ["text", "json", "html"],
