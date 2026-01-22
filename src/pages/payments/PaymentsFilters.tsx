4a4e5bc102e19db041308cbd1a569313021d8581
Ayomide Bakare <bakkareayomideo@gmail.com>
2026-01-22 21:22:57 +0000

    add currency filter

--- src/pages/payments/PaymentsFilters.tsx
+++ src/pages/payments/PaymentsFilters.tsx
@@ -3,18 +3,20 @@ import {
   FilterRow,
   SearchButton,
   SearchInput,
+  Select,
 } from "@/components/ui";
 import { I18N } from "@/constants/i18n";
 import { ChangeEventHandler, useState } from "react";
 import type { PaymentFilter } from "@/types/payment";
 import type { PaymentsFiltersProps } from "./types";
 import { hasActiveFilter } from "@/utilities/filters/hasActiveFilter";
+import { CURRENCIES } from "@/constants";
 
 export const PaymentsFilters = ({
   onFilter,
   defaultValues,
 }: PaymentsFiltersProps) => {
-  const initialFilters = { search: "" };
+  const initialFilters = { search: "", currency: "" };
   const [filters, setFilters] = useState<Partial<PaymentFilter>>({
     ...initialFilters,
     ...defaultValues,
@@ -23,7 +25,9 @@ export const PaymentsFilters = ({
     hasActiveFilter(defaultValues),
   );
 
-  const handleInputChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+  const handleInputChange: ChangeEventHandler<
+    HTMLInputElement | HTMLSelectElement
+  > = (e) => {
     const { name, value } = e.target;
     setFilters((prev) => ({ ...prev, [name]: value }));
   };
@@ -49,6 +53,22 @@ export const PaymentsFilters = ({
         placeholder={I18N.SEARCH_PLACEHOLDER}
         aria-label={I18N.SEARCH_LABEL}
       />
+      <Select
+        name="currency"
+        role="combobox"
+        value={filters.currency}
+        onChange={handleInputChange}
+        aria-label={I18N.CURRENCY_FILTER_LABEL}
+      >
+        <option value="" selected disabled>
+          {I18N.CURRENCIES_OPTION}
+        </option>
+        {CURRENCIES.map((currency) => (
+          <option key={currency} value={currency}>
+            {currency}
+          </option>
+        ))}
+      </Select>
       <SearchButton type="button" onClick={handleFilter}>
         {I18N.SEARCH_BUTTON}
       </SearchButton>
