1635ebf2971442c694319e3121f750ec642ad965
Ayomide Bakare <bakkareayomideo@gmail.com>
2026-01-22 20:54:55 +0000

    show alert with error message

--- src/pages/payments/Payments.tsx
+++ src/pages/payments/Payments.tsx
@@ -1,14 +1,20 @@
+import type { PaymentFilter } from "@/types/payment";
 import { DataTable } from "@/components/DataTable";
+import { ErrorBox } from "@/components/ui";
+import { isErrorResult } from "@/utilities/errors/isErrorResult";
 import { useGetPayments } from "../../hooks/useGetPayments";
 import { PAYMENTS_COLUMNS } from "./columns";
-import { PaymentFilter } from "@/types/payment";
 
 type PaymentsProps = {
   filters: Partial<PaymentFilter>;
 };
 
 export const Payments = ({ filters }: PaymentsProps) => {
-  const { payments } = useGetPayments(filters);
+  const result = useGetPayments(filters);
 
-  return <DataTable data={payments} columns={PAYMENTS_COLUMNS} />;
+  if (isErrorResult(result)) {
+    return <ErrorBox role="alert">{result.error.message}</ErrorBox>;
+  }
+
+  return <DataTable data={result.payments} columns={PAYMENTS_COLUMNS} />;
 };
