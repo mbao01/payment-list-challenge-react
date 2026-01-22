1c8cdb0b8f3e136da96afe683a54a4f6090ba53d
Ayomide Bakare <bakkareayomideo@gmail.com>
2026-01-22 21:10:25 +0000

    handle 500 reponse and show custom message

--- src/utilities/errors/handleApiError.ts
+++ src/utilities/errors/handleApiError.ts
@@ -2,14 +2,18 @@ import { I18N } from "@/constants/i18n";
 import { ApiError } from "@/types/error";
 import { StatusCodes } from "http-status-codes";
 
+const ERROR_MESSAGE = {
+  [StatusCodes.NOT_FOUND]: I18N.PAYMENT_NOT_FOUND,
+  [StatusCodes.INTERNAL_SERVER_ERROR]: I18N.INTERNAL_SERVER_ERROR,
+} satisfies Partial<Record<StatusCodes, string>>;
+
 export const handleApiError = async (error: unknown): Promise<ApiError> => {
   try {
     if (error instanceof Response) {
       const data = await error.json();
       const message =
-        error.status === StatusCodes.NOT_FOUND
-          ? I18N.PAYMENT_NOT_FOUND
-          : data.message;
+        ERROR_MESSAGE[error.status as keyof typeof ERROR_MESSAGE] ||
+        data.message;
 
       return { message };
     }
