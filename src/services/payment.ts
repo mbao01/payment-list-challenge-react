9e05e489f680e91659d04d2168194ba7ea155171
Ayomide Bakare <bakkareayomideo@gmail.com>
2026-01-22 18:23:27 +0000

    add payment type and useGetPayments hook

--- src/services/payment.ts
+++ src/services/payment.ts
@@ -1,11 +1,7 @@
 import { API_URL } from "@/constants";
+import type { PaymentFilter } from "@/types/payment";
 
-type GetPaymentsServiceArgs = Partial<{
-  search: string;
-  currency: string;
-  page: number;
-  pageSize: number;
-}>;
+type GetPaymentsServiceArgs = Partial<PaymentFilter>;
 
 export const getPaymentsService = async (params?: GetPaymentsServiceArgs) => {
   const searchParams = new URLSearchParams(params as Record<string, string>);
