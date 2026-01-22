6df7402da3c0e4225879ce0ac1edbe818ceca4d6
Ayomide Bakare <bakkareayomideo@gmail.com>
2026-01-22 18:56:33 +0000

    formatDate - support date string

--- src/utilities/dates/formatDate.test.ts
+++ src/utilities/dates/formatDate.test.ts
@@ -16,6 +16,11 @@ describe("formatDate", () => {
     expect(formatDate(date)).toBe("16/04/2024, 09:30:00");
   });
 
+  it("should format a date string", () => {
+    expect(formatDate("2024-04-16T09:30:00Z")).toBe("16/04/2024, 09:30:00");
+    expect(formatDate("2024-04-16")).toBe("16/04/2024, 00:00:00");
+  });
+
   it("should format a date with a custom format", () => {
     const date = new Date("2022-01-01");
 
@@ -26,6 +31,5 @@ describe("formatDate", () => {
     expect(formatDate(null)).toBe(null);
     expect(formatDate(undefined)).toBe(null);
     expect(formatDate("invalid date")).toBe(null);
-    expect(formatDate("2024-04-16")).toBe(null);
   });
 });
