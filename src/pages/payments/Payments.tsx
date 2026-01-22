20564aa26d9ffefba4e4463c717a2d18d05ca264
Ayomide Bakare <bakkareayomideo@gmail.com>
2026-01-22 20:24:25 +0000

    exclude filters from suspense

--- src/pages/payments/Payments.tsx
+++ src/pages/payments/Payments.tsx
@@ -1,18 +1,14 @@
 import { DataTable } from "@/components/DataTable";
 import { useGetPayments } from "../../hooks/useGetPayments";
 import { PAYMENTS_COLUMNS } from "./columns";
-import { PaymentsFilters } from "./PaymentsFilters";
-import { useState } from "react";
 import { PaymentFilter } from "@/types/payment";
 
-export const Payments = () => {
-  const [filters, setFilters] = useState<Partial<PaymentFilter>>({});
+type PaymentsProps = {
+  filters: Partial<PaymentFilter>;
+};
+
+export const Payments = ({ filters }: PaymentsProps) => {
   const { payments } = useGetPayments(filters);
 
-  return (
-    <>
-      <PaymentsFilters onFilter={(filters) => setFilters(filters)} />
-      <DataTable data={payments} columns={PAYMENTS_COLUMNS} />
-    </>
-  );
+  return <DataTable data={payments} columns={PAYMENTS_COLUMNS} />;
 };
