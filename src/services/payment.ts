865f89cb62771711c082a11a82c16832159c0dd4
Ayomide Bakare <bakkareayomideo@gmail.com>
2026-01-22 20:44:52 +0000

    handle api error in service layer

--- src/services/payment.ts
+++ src/services/payment.ts
@@ -1,11 +1,22 @@
 import { API_URL } from "@/constants";
-import type { PaymentFilter } from "@/types/payment";
+import type { PaymentFilter, PaymentsResponse } from "@/types/payment";
+import { handleApiError } from "@/utilities/errors/handleApiError";
 
 type GetPaymentsServiceArgs = PaymentFilter;
 
 export const getPaymentsService = async (params?: GetPaymentsServiceArgs) => {
-  const searchParams = new URLSearchParams(params as Record<string, string>);
+  try {
+    const searchParams = new URLSearchParams(params as Record<string, string>);
 
-  const response = await fetch(`${API_URL}?${searchParams}`);
-  return response.json();
+    const response = await fetch(`${API_URL}?${searchParams}`);
+
+    if (!response.ok) {
+      throw response;
+    }
+
+    return response.json() as Promise<PaymentsResponse>;
+  } catch (error) {
+    const apiError = await handleApiError(error);
+    return { error: apiError };
+  }
 };
